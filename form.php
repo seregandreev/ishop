3776ffcacf7ffcaf0fd393ad0f80dece620e46b6
rail.mingaliev <rail.mingaliev@dn.ru>
2021-12-16 19:57:40 +0300

    форма создания пользователя

--- form.php
+++ form.php
@@ -1,6 +1,20 @@
 <?php
 
-$name = $_POST['name'];
-$age = $_POST['age'];
+$user = $password = 'root';
+$pdo = new Pdo('mysql:dbname=fullstack2;host=127.0.0.1;port=3307', $user, $password);
 
-echo "Hello, $name, you are $age years old!";
+$name = $_POST['name'];
+$login = $_POST['login'];
+$password = $_POST['password'];
+$city_id = $_POST['city_id'];
+
+$query = "INSERT INTO users (name, login, password, city_id) VALUES(:name, :login, :password, :city_id)";
+$res = $pdo->prepare($query);
+$res->execute([
+    ':name' => $name,
+    ':login' => $login,
+    ':password' => $password,
+    ':city_id' => $city_id,
+]);
+
+header('Location: index.php');
